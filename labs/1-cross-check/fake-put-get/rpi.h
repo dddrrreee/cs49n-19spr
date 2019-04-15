614e4a80042f24e1cf8745b38caff0420309e1fc
engler <ddd.rrr.eee@gmail.com>
2019-04-15 12:43:06 -0700

    added enums.

--- labs/1-cross-check/fake-put-get/rpi.h
+++ labs/1-cross-check/fake-put-get/rpi.h
@@ -5,4 +5,21 @@
 void put32(volatile void *addr, unsigned v);
 unsigned get32(volatile void *addr);
 
+
+// different functions we can set GPIO pins to.
+enum {
+    GPIO_FUNC_INPUT   = 0,
+    GPIO_FUNC_OUTPUT  = 1,
+    GPIO_FUNC_ALT0    = 4,
+    GPIO_FUNC_ALT1    = 5,
+    GPIO_FUNC_ALT2    = 6,
+    GPIO_FUNC_ALT3    = 7,
+    GPIO_FUNC_ALT4    = 3,
+    GPIO_FUNC_ALT5    = 2,
+};
+
+// set GPIO function for <pin> (input, output, alt...).  settings for other
+// pins should be unchanged.
+void gpio_set_function(unsigned pin, int function);
+
 #endif
