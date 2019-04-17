40cbb44a69c5a80a527790a75191745c2dc9fa0a
engler <ddd.rrr.eee@gmail.com>
2019-04-17 16:28:29 -0700

    minor

--- libpi-fake/fake-pi.c
+++ libpi-fake/fake-pi.c
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+// not very good random.
+unsigned short bad_random(void) {
+    static unsigned short lfsr = 0xACE1u;
+    static unsigned bit;
+    bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+    lfsr =  (lfsr >> 1) | (bit << 15);
+    return lfsr;
+}
+
+
+
 void uart_init(void) { 
     trace("uart\n");
 }
